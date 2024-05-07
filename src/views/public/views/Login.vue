a844cc6e342484c69c839a4cdd32a7713dabfab9
joaquin Reyes <joaq.reyesm0@gmail.com>
2024-05-07 16:26:34 -0400

    Inicio del proyecto

---
+++ src/views/public/views/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div
+      class="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0"
+    >
+      <!-- left side -->
+      <div class="flex flex-col justify-center p-8 md:p-14">
+        <span class="mb-3 text-4xl font-bold">DoobloReports</span>
+        <span class="font-light text-gray-400 mb-8">
+          Bienvenido al sistema de reportes de Dooblo creado por Datavoz
+        </span>
+        <div class="py-4">
+          <span class="mb-2 text-md">Email</span>
+          <input
+            type="text"
+            class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+            name="email"
+            id="email"
+          />
+        </div>
+        <div class="py-4">
+          <span class="mb-2 text-md">Contraseña</span>
+          <input
+            type="password"
+            name="pass"
+            id="pass"
+            class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+          />
+        </div>
+        <div class="flex justify-between w-full py-4">
+          <span class="font-bold text-md">Olvidé la contraseña</span>
+        </div>
+        <button
+          class="w-full bg-violet-500 text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300"
+        >
+          Ingresar
+        </button>
+      </div>
+      <!-- {/* right side */} -->
+      <div class="relative">
+        <img
+          src="../../../assets/img/Logo.jpg"
+          alt="img"
+          class="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
+        />
+        <!-- text on image  -->
+        <div
+          class="absolute hidden bottom-10 right-6 p-6 bg-white bg-opacity-30 backdrop-blur-sm rounded drop-shadow-lg md:block"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+</style>
