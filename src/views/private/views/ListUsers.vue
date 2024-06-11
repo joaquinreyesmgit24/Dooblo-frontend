05ffb03bb94af04072eb8f790885c4f7c0c340bc
joaquin Reyes <joaq.reyesm011@gmail.com>
2024-06-11 17:09:52 -0400

    Avance de la página web 11-06-2024

--- src/views/private/views/ListUsers.vue
+++ src/views/private/views/ListUsers.vue
@@ -1,111 +1,157 @@
 <template>
-        <div class="grid grid-cols-1 gap-6 mb-6">
-                <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5">
-                    <div class="flex justify-between mb-6">
-                        <div>
-                            <div class="text-2xl font-semibold mb-1">Usuarios</div>
-                                <VueGoodTable :columns="columns" :rows="rows" :search-options="searchOptions"
-                                    :pagination-options="paginationOptions" max-height="450px" :fixed-header="true">
-                                        <!-- <template v-slot:table-row="props">
-                                            <span v-if="props.column.field == 'acciones'
-                                            ">
-                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalUpdateUser"
-                                                @click="openModal(props.row)">
-                                                Editar
-                                            </button>
-                                            </span>
-                                            <span v-if="props.column.field == 'rol'">
-                                            {{ props.row.rol.nombre }}
-                                            </span>
-                                        </template>
-                                        <template v-slot:emptystate>
-                                            <div style="text-align: center;">
-                                            No hay datos disponibles
-                                            </div>
-                                    </template> -->
-                            </VueGoodTable>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            </a>
+            <button data-collapse-toggle="navbar-dropdown" type="button"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-dropdown" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+            </button>
+            <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+                <ul
+                    class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                            aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                            class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown
+                            <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m1 1 4 4 4-4" />
+                            </svg></button>
+                        <!-- Dropdown menu -->
+                        <div id="dropdownNavbar"
+                            class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-400"
+                                aria-labelledby="dropdownLargeButton">
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                                </li>
+                            </ul>
+                            <div class="py-1">
+                                <a href="#"
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
+                                    out</a>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
+    </nav>
 </template>
 
 <script>
-import GlobalService from "../../../services/GlobalServices";
-import { VueGoodTable } from "vue-good-table-next";
-import "vue-good-table-next/dist/vue-good-table-next.css";
-import { useToast } from "vue-toastification";
-import dayjs from "dayjs"; // Importa dayjs
-export default {
-    name:"list-users",
-    components: {
-    VueGoodTable,
-},
-    data(){
-        return{
-            searchOptions: {
-            enabled: true,
-            placeholder: "Buscar",
+    import GlobalService from "../../../services/GlobalServices";
+    import { VueGoodTable } from "vue-good-table-next";
+    import "vue-good-table-next/dist/vue-good-table-next.css";
+    import { useToast } from "vue-toastification";
+    import dayjs from "dayjs"; // Importa dayjs
+    export default {
+        name: "list-users",
+        components: {
+            VueGoodTable,
+        },
+        data() {
+            return {
+                searchOptions: {
+                    enabled: true,
+                    placeholder: "Buscar",
+                },
+                paginationOptions: {
+                    enabled: true,
+                    perPage: 5,
+                    nextLabel: "Siguiente",
+                    prevLabel: "Anterior",
+                    rowsPerPageLabel: "Filas por página",
+                    paginationComplete: false,
+                    ofLabel: "de",
+                },
+                toast: useToast(),
+                columns: [
+                    {
+                        label: "Nombre de usuario",
+                        field: "username",
+                    },
+                    {
+                        label: "Rol",
+                        field: "role",
+                    },
+                    {
+                        label: "Fecha de creación",
+                        field: "date",
+                        type: "date",
+                        dateInputFormat: "dd-MM-yyyy HH:mm:ss",
+                        dateOutputFormat: "dd-MM-yyyy",
+                    },
+                    {
+                        label: "Estado",
+                        field: "status",
+                    },
+                    {
+                        label: "Acciones",
+                        field: "acciones",
+                        type: "slots",
+                    },
+                ],
+                rows: [],
+            };
+        },
+        mounted() {
+            this.getDataUsers();
+        },
+        methods: {
+            getDataUsers() {
+                GlobalService.getData("/auth/listar-usuarios")
+                    .then((response) => {
+                        console.log(response.users);
+                        this.rows = response.users.map((user) => ({
+                            id: user.id,
+                            username: user.username,
+                            role: user.role,
+                            date: dayjs(user.createdAt).format("DD-MM-YYYY HH:mm:ss"),
+                            status: user.status ? "Activo" : "Inactivo",
+                        }));
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
             },
-            paginationOptions: {
-            enabled: true,
-            perPage: 5,
-            nextLabel: "Siguiente",
-            prevLabel: "Anterior",
-            rowsPerPageLabel: "Filas por página",
-            paginationComplete: false,
-            ofLabel: "de",
-            },
-            toast: useToast(),
-            columns: [{
-            label: "Nombre de usuario",
-            field: "username",
-          },
-          {
-            label: "Rol",
-            field: "rol",
-          },
-          {
-            label: "Fecha de creación",
-            field: "fecha",
-            type: "date",
-            dateInputFormat: "dd-MM-yyyy HH:mm:ss",
-            dateOutputFormat: "dd-MM-yyyy",
-          },
-          {
-            label: "Estado",
-            field: "estado",
-          },
-          {
-            label: "Acciones",
-            field: "acciones",
-            type: "slots",
-          }],
-            rows:[]
-        }
-    },
-    mounted(){
-        this.getDataUsers()
-    },
-    methods:{
-    getDataUsers() {
-        GlobalService.getData("/auth/listar-usuarios")
-            .then((response) => {
-            console.log(response.users);
-            this.rows = response.users.map((user) => ({
-                id: user.id,
-                username: user.username,
-                role: user.role,
-                date: dayjs(user.createdAt).format("DD-MM-YYYY HH:mm:ss"),
-                status: user.status ? "Activo" : "Inactivo",
-            }));
-            })
-            .catch((error) => {
-            console.log(error);
-            });
-    },
-    }
-}
+        },
+    };
 </script>
 
 <style>
