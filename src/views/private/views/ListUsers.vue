2dfebd1e3c35ded56931332cb42fbe431a7845de
joaquin Reyes <joaq.reyesm011@gmail.com>
2024-06-12 12:01:31 -0400

    Avance de la página web 12-06-2024 v2 con el modal incluido

--- src/views/private/views/ListUsers.vue
+++ src/views/private/views/ListUsers.vue
@@ -4,30 +4,49 @@
             <div class="flex justify-between mb-6">
                 <div>
                     <div class="text-2xl font-semibold mb-1">Usuarios</div>
-                        <VueGoodTable :columns="columns" :rows="rows" :search-options="searchOptions"
-                            :pagination-options="paginationOptions" max-height="450px" :fixed-header="true">
-                                <template v-slot:table-row="props">
-                                    <span v-if="props.column.field == 'acciones'
+                    <VueGoodTable :columns="columns" :rows="rows" :search-options="searchOptions"
+                        :pagination-options="paginationOptions" max-height="450px" :fixed-header="true">
+                        <template v-slot:table-row="props">
+                            <span v-if="props.column.field == 'acciones'
                                     ">
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalUpdateUser"
-                                        @click="openModal(props.row)">
-                                        Editar
-                                    </button>
-                                    </span>
-                                    <span v-if="props.column.field == 'role'">
-                                    {{ props.row.role.name }}
-                                    </span>
-                                </template>
-                                <template v-slot:emptystate>
-                                    <div style="text-align: center;">
-                                    No hay datos disponibles
-                                    </div>
-                            </template>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#modalUpdateUser" @click="openModal(props.row)">
+                                    Editar
+                                </button>
+                            </span>
+                            <span v-if="props.column.field == 'role'">
+                                {{ props.row.role.name }}
+                            </span>
+                        </template>
+                        <template v-slot:emptystate>
+                            <div style="text-align: center;">
+                                No hay datos disponibles
+                            </div>
+                        </template>
                     </VueGoodTable>
+                    <div v-if="showModal" class="fixed z-10 inset-0 overflow-y-auto">
+                        <div class="flex items-center justify-center min-h-screen">
+                            <div class="fixed inset-0 transition-opacity" @click="closeModal" aria-hidden="true">
+                                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                            </div>
+                            <div class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full"
+                                role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                                <div class="px-6 py-4">
+                                    <!-- Contenido del modal -->
+                                    <h2 class="text-lg font-bold mb-2">Mi Modal</h2>
+                                    <p>Este es el contenido de mi modal.</p>
+                                </div>
+                                <div class="px-6 py-4 bg-gray-100">
+                                    <button @click="closeModal"
+                                        class="px-4 py-2 bg-gray-500 text-white rounded">Cerrar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-</div>
+    </div>
 </template>
 
 <script>
@@ -43,6 +62,7 @@
         },
         data() {
             return {
+                showModal: false,
                 searchOptions: {
                     enabled: true,
                     placeholder: "Buscar",
@@ -90,6 +110,12 @@
             this.getDataUsers();
         },
         methods: {
+            openModal(){
+                this.showModal = true;
+            },
+            closeModal() {
+                this.showModal = false;
+            },
             getDataUsers() {
                 GlobalService.getData("/auth/listar-usuarios")
                     .then((response) => {
